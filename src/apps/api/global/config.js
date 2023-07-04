25d38d0a4ba2cb825a8e5321a95dba5b1d633148
AjiPerdana38 <ajicooljazz38@gmail.com>
2023-07-04 16:40:30 +0700

    update endpoint api

--- src/apps/api/global/config.js
+++ src/apps/api/global/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  BASE_URL: 'https://pptik-local.pptik.id:5525',
+  BASE_URL: 'https://camera.pptik.id/api',
   BASE_URL_V2: 'https://smartparking.pptik.id/api/v1/devices/result'
 }
 
